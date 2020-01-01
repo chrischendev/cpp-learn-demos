ffde34bedc45c6dab9e5ca5770e704c29efb7d76
chris <chrischen2018@163.com>
2020-01-01 18:26:33 +0800

    mod

--- cpp-tech-demo/test/IoTest.cpp
+++ cpp-tech-demo/test/IoTest.cpp
@@ -19,9 +19,9 @@ void IoTest::mainTest() {
  * 测试写入文件
  */
 void IoTest::test01() {
-    ofstream outfile("D:\\temp\\file\\cpp_io_test_02.txt", ios::app);
+    ofstream outfile("F:\\Temp\\file\\cpp_io_test_02.txt", ios::app);
 
-    string data = "i love sunfeifei.";
+    string data = "L love sunfeifei.";
 
     outfile << data << endl;
 }
@@ -30,7 +30,7 @@ void IoTest::test01() {
  * 测试读取文件
  */
 void IoTest::test02() {
-    fstream infile("D:\\temp\\file\\cpp_io_test_01.txt", ios::in);
+    fstream infile("F:\\Temp\\file\\cpp_io_test_01.txt", ios::in);
 
     char buffer[1024];
     infile.read(buffer, sizeof(buffer) * sizeof(char));
